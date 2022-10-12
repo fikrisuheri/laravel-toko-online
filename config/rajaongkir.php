f886692ed93d4c001b56c09ebdbf34c272dac427
Al Amrikasir <amrikasir@gmail.com>
2022-10-12 15:14:49 +0700

    rajaongkir config

---
+++ config/rajaongkir.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    /*
+     * Atur API key yang dibutuhkan untuk mengakses API Raja Ongkir.
+     * Dapatkan API key dengan mengakses halaman panel akun Anda.
+     */
+    'api_key' => env('RAJAONGKIR_API_KEY'),
+
+    /*
+     * Atur tipe akun sesuai paket API yang Anda pilih di Raja Ongkir.
+     * Pilihan yang tersedia: ['starter', 'basic', 'pro'].
+     */
+    'package' => env('RAJAONGKIR_PACKAGE', 'starter'),
+];
