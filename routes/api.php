32b6dd41231e3b44d5c6099835b1127872789811
Al Amrikasir <amrikasir@gmail.com>
2023-05-29 10:49:12 +0700

    creating api

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+/**
+ * create api based on route web
+ */
